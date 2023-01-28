522914ff770e96c7acce4e7bc76aaa42c8885e99
Danila Romanov <danila.romanof@gmail.com>
2023-01-28 20:57:37 +0200

    Missing files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {LoginComponent} from "./login/login.component";
 import {RegistrationComponent} from "./registration/registration.component";
+import {UserItemsComponent} from "./user-items/user-items.component";
 
 const routes: Routes = [
   {path: "home", component: HomeComponent},
   {path: "login", component: LoginComponent},
   {path: "registration", component: RegistrationComponent},
+  {path: "items", component: UserItemsComponent},
   {path: "**", redirectTo: "/home"}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,18 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import { RegistrationComponent } from './registration/registration.component';
+import { UserItemsComponent } from './user-items/user-items.component';
+import {JwtInterceptor} from "./jwt.interceptor";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    UserItemsComponent
   ],
   imports: [
     BrowserModule,
@@ -23,7 +26,13 @@ import { RegistrationComponent } from './registration/registration.component';
     HttpClientModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
